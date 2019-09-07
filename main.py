2c1c8d251db68bb6aee25057b725eb45ea6ef93f
Rakibul Yeasin <rytotul@gmail.com>
2019-09-07 13:52:46 +0600

    Tested all the functions and they are working fine :fire:

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import sys
 # Custom Modules
 from api.SubScene import SubScene
 
+# Prohbit system from writing byte-codes
+sys.dont_write_bytecode = True
+
 # The Traditional main() function
 def main():
-    pass
+    sub = SubScene() # Initialize the api Class
+    detail = {
+        'name': 'Hello',
+        'year': '2008'
+    }
+    link = sub.getDetail(detail) # Available Subtitles
+    print(link)
+    links = sub.getSubLink(link) # Link to Specific Subtitle
+    print(links)
+    down = sub.getDownLink(links[0]) # DownLoad link for Specific Language
+    print(down)
 
 if __name__ == '__main __':
     main()
