5810400c7afaec2f3944d5b33e69fea104f3759a
Rakibul Yeasin <rytotul@gmail.com>
2019-09-07 13:51:43 +0600

    Added Support to Extract Download Link :heart:

--- api/SubScene.py
+++ api/SubScene.py
@@ -3,20 +3,29 @@ import time
 from requests import get, post
 from bs4 import BeautifulSoup
 
+# Prohbit system from writing byte-codes
+sys.dont_write_bytecode = True
+
 class SubScene:
     # Subscene api
+
     def __init__(self):
         self.name = __name__
         self.link = 'https://subscene.com'
+        self.lang = 'English'
 
     def getDetail(self, detail):
         # Search for a specific movie
         # and scrape intended one from the list
+
         try:
             data = {
+                # We have got detail as a list
                 'query': detail['name']
             }
+            
             while True:
+                # Loop till we don't get any answer from server
                 search = post(self.link + '/subtitles/searchbytitle', data=data)
                 # print(search.status_code)
                 if search.status_code != 200:
@@ -28,27 +37,63 @@ class SubScene:
             div = soup.find('div', {'class': 'search-result'})
             h2 = div.findAll('h2')
             ul = div.findAll('ul')
-            links = []
+            links = [] # All the links will be stored here
 
             try:
-                if h2[0]['class'][0].title() == 'Exact':
+                if h2[0]['class'][0].title() == 'Exact': # Use the links under 'Exact' heading
                     if len(ul[0].findAll('a')) > 1:
                         for a in ul[0].findAll('a'):
                             links.append(self.link+a.get('href'))
-                            if detail.get('year') != '' and detail.get('year') in a.text:
-                                return links[-1]
-                        else:
-                            return self.getFromFullChoice(h2, ul, detail)
-                    else:
-                        if detail.get('year') != '' and detail.get('year') in ul[0].find('a').text:
-                            return self.link + ul[0].find('a')['href']
-                        else:
-                            return self.getFromFullChoice(h2, ul, detail)
-                else:
-                    return self.getFromFullChoice(h2, ul, detail)
+                            if detail['year'] != '' and detail['year'] in a.text:
+                                return links[0]
+                        
             except Exception as e:
                 print(e)
                 return None
+
         except Exception as e:
             print(e)
             return None
+
+    def getSubLink(self, link):
+        # Get Direct Link for Downloading Subtitle
+        # Link is the Search Results page link
+
+        html = get(link).text
+        soup = BeautifulSoup(html, 'html.parser')
+        l = soup.find('tbody').findAll('a')
+        langs,subs,links=[],[],[]
+
+        for a in l:
+            al = a.findAll('span')
+            try:
+                la = al[0].text.strip()
+                if la == self.lang or la == 'English':
+                    langs.append(la)
+                    subs.append(al[1].text.strip())
+                    links.append(self.link+a['href'])
+            except Exception:
+                pass
+        
+        for l,s,li in zip(langs, subs, links):
+                if l == self.lang: # and 'positive-icon' in a.span['class']
+                    # print(f'Subtitle name: {s}\nLink: {links}')
+                    return links
+        # return None
+    
+
+    def getDownLink(self, link):
+        html=get(link).text
+        soup = BeautifulSoup(html, 'html.parser')
+        s = soup.find('div', {'class': 'download'})
+        return self.link + s.a['href']
+
+# sub = SubScene() # Initialize the api Class
+# detail = {
+#     'name': 'Hello',
+#     'year': '2008'
+# }
+# link = sub.getDetail(detail)
+# links = sub.getSubLink(link)
+# down = sub.getDownLink(links[0])
+# print(down)
